15a956ab8b42654d4617b297f9ef54952be219a3
R. Kukuh <rkukuh@gmail.com>
2020-04-28 11:21:55 +0700

    Initial Commit

---
+++ Struct and Class/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Struct and Class
+//
+//  Created by R. Kukuh on 28/04/20.
+//  Copyright © 2020 R. Kukuh. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
